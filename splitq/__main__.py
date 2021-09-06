ca79c7c69f019ba1f37b0b19f4e33ba7b51d3d9f
CooperStansbury <cstansbu@umich.edu>
2021-09-06 09:58:27 -0400

    update sequence splitting and configure outputs

--- splitq/__main__.py
+++ splitq/__main__.py
@@ -2,9 +2,28 @@ import sys
 import os
 from datetime import datetime
 import argparse
+import pandas as pd
+
+from Bio import Restriction
+from Bio.Seq import Seq
+from Bio import SeqIO
+
+def partition(alist, indices):
+    """A function to split a list based on item indices 
+    
+    Parameters:
+    -----------------------------
+        : alist (list): a list to be split
+        : indices (list): list of indices on which to divide the input list
+        
+    Returns:
+    -----------------------------
+        : splits (list): a list of subreads based on cut sites
+    """
+    return [alist[i:j] for i, j in zip([0]+indices, indices+[None])]
 
 
-def split_fastq(infile):
+def split_fastq(infile, N=300):
     """A function to digest the reads by a cut site prior to mapping 
     against the reference.
 
@@ -15,51 +34,76 @@ def split_fastq(infile):
     Parameters:
     -----------------------------
         : infile (str): path to the input fastq file
-        : outfile (str): path to the output fastq file
+        : N (int): number of base pairs surrounding the cut-site to define as pairs
+        
+        
+    Returns:
+    -----------------------------
+        : pairs_table (pd.DataFrame): a dataframe with subread pairs and metadata
     """
-    output_handle = open(outfile, "w")
     handle = open(infile, "r")
     records = list(SeqIO.parse(handle, "fastq"))
-
     handle.close()
+    
+    new_rows = []
 
+    # iterate through each reaa of the fastq file
     for record in records:
+        
+        # digest read on NlaIII sites
         restriction_site_len = len(Restriction.NlaIII.site)
         cut_sites = Restriction.NlaIII.search(record.seq)
-
-        print(cut_sites)
-        break
-
-        if len(cut_sites) == 0:
-            output_handle.write(record.format("fastq"))
-        else :
-            subread_counter = 0
-
-            for i in range(0, len(cut_sites)):
-                if i == 0:
-                    subread = record[0 : cut_sites[i]-1]
-                else:
-                    subread = record[cut_sites[i-1]-1 : cut_sites[i]-1]
-
-                if len(subread.seq) <= 4:
-                    continue
-
-                subread.id += "/0" + str(subread_counter)
-                subread.name += "/0" + str(subread_counter)
-                subread.description += "/0" + str(subread_counter)
-                subread_counter += 1
-                output_handle.write(subread.format("fastq"))
-
-            subread = record[cut_sites[i]-1 :]
-            subread.id += "/0" + str(subread_counter)
-            subread.name += "/0" + str(subread_counter)
-            subread.description += "/0" + str(subread_counter)
-
-            if len(subread.seq) <= 4:
-                continue
-
-            output_handle.write(subread.format("fastq"))
-    output_handle.close()
+        cut_sites = [x-1 for x in cut_sites] # cut site indices are the bp AFTER the CATG enzyme
+       
+        if len(cut_sites) > 0:
+            
+            # split the read on NlaIII cites
+            splits = partition(record, cut_sites)
+        
+            # loop through pairs of subreads (i, i+1)
+            subread_count = 0
+            for idx in range(len(splits) - 1):
+                subread_count += 1
+                
+                left_read = splits[idx]
+                right_read = splits[idx + 1]
+                
+                # trim thye CATG sequence from the reads
+                if left_read.seq.endswith("CATG"):
+                    left_read = left_read[:-4]
+                    
+                if right_read.seq.endswith("CATG"):
+                    right_read = right_read[:-4]
+                
+                # trim subreads based on parameter N
+                if len(left_read) > N:
+                    left_read = left_read[-N:]
+                    
+                if len(right_read) > N:
+                    right_read = right_read[:N]
+                
+                # get read metadata from left read - assume consistent between subreads
+                metadata = dict(item.split("=") for item in left_read.description.split(" ") if "=" in item)
+                
+                # construct new data record
+                new_record = {
+                    'read_id' : left_read.id,
+                    'subread_id' : subread_count,
+                    'left_read' : "".join(left_read.seq),
+                    'right_read' : "".join(right_read.seq),
+                    'subread_max_length' : N,
+                }
+                
+                # add metadata
+                for k, v in metadata.items():
+                    new_record[k] = v
+                    
+                new_rows.append(new_record)
+        
+    # build pairs table 
+    pairs_table = pd.DataFrame(new_rows)
+    return pairs_table
+        
 
 
 if __name__ == '__main__':
@@ -73,16 +117,20 @@ if __name__ == '__main__':
 
     parser.add_argument("-i", 
                         help="Input merged fastq file.")
-    parser.add_argument("-od", 
+    parser.add_argument("-n", 
+                        nargs='?',
+                        default=300,
+                        help="Number of base pairs surrounding a cut site.")
+    parser.add_argument("-o", 
                         nargs='?',
                         type=str, 
                         default="results/",
                         help="The path to a directory for all output files.")
     parser.add_argument("-overwrite", 
-                    nargs='?',
-                    type=bool, 
-                    default=True,
-                    help="If True, overwrite the directory at `-od`")
+                        nargs='?',
+                        type=bool, 
+                        default=True,
+                        help="If True, overwrite the directory at `-od`")
   
     ############################################################################################
     # INPUT ARGUMENT PARSING
@@ -91,7 +139,8 @@ if __name__ == '__main__':
     
     # argument parsing - may need to handle more robustly
     FASTQ = args.i
-    OUTPUT_DIR = args.od
+    N = int(args.n)
+    OUTPUT_DIR = args.o
     OVERWRITE = args.overwrite
     
     # make output dir, handle overwriting flag
@@ -101,13 +150,17 @@ if __name__ == '__main__':
     # DATA PROCESSING
     ############################################################################################
     
-    """TODO: finish this function, perform QA, and write out new fastq file to the newly created
-    directory"""
-    
-    split_fastq(FASTQ)
+    # generate pairs
+    pairs_table = split_fastq(FASTQ, N)
+        
+    # get input file basename and generate output filename 
+    base = os.path.basename(FASTQ)
+    basename = os.path.splitext(base)[0]
+    new_filename = f"{OUTPUT_DIR}{basename}_PAIRS.csv"
     
     ############################################################################################
     # OUTPUTS
     ############################################################################################
     
-    """TODO: store outputs"""
+    pairs_table.to_csv(new_filename, index=False)
+    print(f"Done saving: `{new_filename}`")
